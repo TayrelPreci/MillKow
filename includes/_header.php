1ab7f1ce2c48cae1d9a50c52e58b3aafcd9162f6
Tayrel Preci Rodrigues <93671399+TayrelPreci@users.noreply.github.com>
2022-08-18 23:47:51 -0300

    commit 0.3
    commit main

--- includes/_header.php
+++ includes/_header.php
@@ -5,17 +5,13 @@
     <img id='Logo' class='Logo' src="imagens/Logo_Millkow.png"></img>
 
     <div id='Block2' class='Block2'>
-        
-        <ul>
-    
-            <li class="cowzinha" ><a class="a" href=""><h1 class='texthead'>Vegan's Milk</h1></a></li>
-    
-            <li class="cowzinha" ><a class="a" href=""><h1 class='texthead'>Animal's Milk</h1></a></li>
-    
-            <li class="cowzinha" ><a class="a" href=""><h1 class='texthead'>Contact</h1></a></li>
-    
-        </ul>
 
+        <div class="cowzinha"> <a class="a" href=""> <h1 class='texthead'>Vegan's Milk</h1> </a> </div>   
+    
+        <div class="cowzinha"> <a class="a" href=""> <h1 class='texthead'>Animal's Milk</h1> </a> </div>    
+  
+        <div class="cowzinha"> <a class="a" href=""> <h1 class='texthead'>Contact</h1> </a> </div>    
+    
     </div>
 
 </div>
--- index.php
+++ index.php
@@ -7,7 +7,51 @@ include_once "includes/_header.php";
 
 <div class="body">
 
-    <h1 class="text" style="color: black;">bodyyziin</h1>
+    <div class="boxs">
+
+        <div class="itens">
+            
+            <p style="font-family: modak; font-size: 22px; text-align: center; font-weight: 100; color: #876A27;">Leite de Morango</p>
+    
+            <img src="imagens/leite_morango.png" alt="" style="height: 200px;"> 
+    
+        </div>
+
+        <div class="itens">
+            
+            <p style="font-family: modak; font-size: 22px; text-align: center; font-weight: 100; color: #876A27;">Leite de Morango</p>
+    
+            <img src="imagens/leite_morango.png" alt="" style="height: 200px;"> 
+    
+        </div>
+
+        <div class="itens">
+            
+            <p style="font-family: modak; font-size: 22px; text-align: center; font-weight: 100; color: #876A27;">Leite de Morango</p>
+    
+            <img src="imagens/leite_morango.png" alt="" style="height: 200px;"> 
+    
+        </div>
+
+        <div class="itens">
+            
+            <p style="font-family: modak; font-size: 22px; text-align: center; font-weight: 100; color: #876A27;">Leite de Morango</p>
+    
+            <img src="imagens/leite_morango.png" alt="" style="height: 200px;"> 
+    
+        </div>
+
+        <div class="itens">
+            
+            <p style="font-family: modak; font-size: 22px; text-align: center; font-weight: 100; color: #876A27;">Leite de Morango</p>
+    
+            <img src="imagens/leite_morango.png" alt="" style="height: 200px;"> 
+    
+        </div>
+        
+        
+
+    </div>
 
 </div>
 
